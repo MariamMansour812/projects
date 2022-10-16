dc33d90d98a93d2d3500e9d57c7fbe180d4bffe0
MariamMansour812 <115506364+MariamMansour812@users.noreply.github.com>
2022-10-17 01:53:08 +0200

    Update CODE1.py

--- CODE1.py
+++ CODE1.py
@@ -1,6 +1,15 @@
+# find the max number
 def max(x,y):
   if x>=y:
     retrun x
   else:
     retrun y
 print(max(8,12))
+#######################
+# find the fectorial
+def factorial(n):
+    fact = 1
+    for i in range(1,n+1):
+        fact=fact*i
+    return fact
+print(factorial(6))
